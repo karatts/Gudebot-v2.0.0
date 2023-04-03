06ec692732c4a5bd061dccbde5ff61d3751212c6
Glitch (catkin-furry-hide) <none>
2023-04-03 15:43:32 +0000

    🍶🎊 Checkpoint ./commands/pat.js:48400496/658 ./app.js:48400496/8

--- commands/pat.js
+++ commands/pat.js
@@ -1,11 +1,22 @@
 import { EmbedBuilder } from 'discord.js';
 
-export function patEmbed(body){
+
+export function patEmbed(client, body){
+  console.log(client);
+  console.log(body);
+  
   let nickname = body.member.nick ? body.member.nick : body.member.user.username;
   
   if("options" in body.data === true){ // replace name with person
+    let userID = client.users.fetch(body.data.options[0].value);
+    const guild = client.fetchGuildPreview(body.guild_id);
     console.log(body.data.options[0].value);
-    let nickname = body.member.nick ? body.member.nick : body.member.user.username;
+    
+    userID.then(value => {
+      console.log(value.username);
+      nickname = value.username;
+    })
+    
   }
   
   let description = "There there " + nickname + ", everything will be okay.";
