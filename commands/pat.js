798fd9d742fe572e0bf02110084d64b9ec2231f4
Glitch (catkin-furry-hide) <none>
2023-04-03 15:55:32 +0000

    🚇🎆 Checkpoint ./commands/pat.js:48400496/6275

--- commands/pat.js
+++ commands/pat.js
@@ -1,30 +1,6 @@
 import { EmbedBuilder } from 'discord.js';
 
-
-export function patEmbed(client, body){
-  console.log(client);
-  console.log(body);
-  
-  let nickname = body.member.nick ? body.member.nick : body.member.user.username;
-  
-  if("options" in body.data === true){ // replace name with person
-    let userID = client.users.fetch(body.data.options[0].value);
-    const guild = client.fetchGuildPreview(body.guild_id);
-    console.log(body.data.options[0].value);
-    
-    userID.then(value => {
-      console.log(value.username);
-      nickname = value.username;
-    })
-    
-  }
-  
-  let description = "There there " + nickname + ", everything will be okay.";
-
-  let embed = new EmbedBuilder()
-      .setColor(0xc55000)
-      .setTitle(description);
-  
+function patImage() {
   let pats = ['https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat1.gif?v=1680504433558', 
               'https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat2.gif?v=1680504726840', 
               'https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat3.gif?v=1680504724348', 
@@ -35,6 +11,29 @@ export function patEmbed(client, body){
               'https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat8.gif?v=1680506237412',
               'https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat9.gif?v=1680506243503',
               'https://cdn.glitch.global/a54f3cb1-081d-4521-bf2c-37769eeaa593/pat10.gif?v=1680506246692'];
+  
+  return pats[(Math.floor(Math.random() * pats.length))];
+}
 
-  return {embed: embed, image: pats[(Math.floor(Math.random() * pats.length))]};
+
+export function patEmbed(client, body){
+  let nickname = body.member.nick ? body.member.nick : body.member.user.username;
+  let embed = new EmbedBuilder()
+    .setColor(0xc55000)
+  
+  if("options" in body.data === true){ // replace name with person
+    let userID = client.users.fetch(body.data.options[0].value);
+    const guild = client.fetchGuildPreview(body.guild_id);
+    console.log(body.data.options[0].value);
+    userID.then(value => { 
+      let description = "There there " + value.username + ", everything will be okay.";
+      embed.setDescription(description);
+      return {embed: embed, image:patImage()};
+    })
+  } else {
+    let description = "There there " + nickname + ", everything will be okay.";
+    embed.setDescription(description);
+    return {embed: embed, image:patImage()};
+  }
+  
 }
